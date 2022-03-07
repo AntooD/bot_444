6cfc07f015652f870b786beb2dfcc85093d419a4
DEHU ANTONIN p2002612 <antonin.dehu@etu.univ-lyon1.fr>
2022-03-06 23:27:00 -0500

    solution de l'issue #1

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const pathToFile = "./media/test.mp4"
 
     const tweet = async () => {
     try {
-        const mediaIdVideo = await rwClient.v1.uploadMedia(pathToFile);
+        const mediaIdVideo = await rwClient.v1.uploadMedia(pathToFile, { mimetype: 'EUploadMimeType.Mp4' },{longvideo:'true'});
         await rwClient.v1.tweet("Je tweet cette vidéo avec Javascript :", { media_ids: mediaIdVideo });
 
             
